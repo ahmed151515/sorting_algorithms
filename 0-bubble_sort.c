8c54623b8a7ef52ad03ae424353197aad9450893
ahmed arafa <ahmedarafa.200444@gmail.com>
2023-12-18 11:02:45 +0200

    .

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -22,6 +22,10 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int flag = 1;
 
+	if (array == NULL || size == 0)
+	{
+		return;
+	}
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
