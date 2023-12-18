be00cee473fd8453d8c190e23897fbf9e0dcb585
ahmed arafa <ahmedarafa.200444@gmail.com>
2023-12-18 13:07:53 +0200

    .

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,12 +11,20 @@ void swap(int *x, int *y)
 	*x = *y;
 	*y = temp;
 }
-
+/**
+ * selection_sort - selection_sort
+ * @array: array
+ * @size: size
+ */
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j;
 	size_t min;
 
+	if (array == NULL || size == 0)
+	{
+		return;
+	}
 	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
